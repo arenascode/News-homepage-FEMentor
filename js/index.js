05a6ed8c572a632ab71620e343570cbb356ef9f3
Miguel Arenas <migue.arenas91@gmail.com>
2023-03-04 17:57:18 -0300

    modal ready

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const btnBurguer = document.getElementById('btnMenu')
+const modal = document.getElementById('myModal')
+const closeModal = document.getElementById('closeModalBton')
+console.log(btnBurguer);
+console.log(modal);
+console.log(closeModal);
+
+btnBurguer.onclick = function () {
+  modal.style.display = "block"
+}
+
+closeModal.onclick = function () {
+  modal.style.display = "none"
+}
