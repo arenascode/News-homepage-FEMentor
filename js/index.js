34c2e805e68872394a0698bcb167ecac187dadef
Miguel Arenas <migue.arenas91@gmail.com>
2023-03-07 17:22:51 -0300

    mobile development done

--- js/index.js
+++ js/index.js
@@ -7,12 +7,23 @@ console.log(closeModal);
 
 btnBurguer.onclick = function () {
   modal.style.display = "block"
+  setTimeout(() => {
+    modal.style.opacity= 1;
+  }, 10)
 }
 
-closeModal.onclick = function () {
+closeModal.onclick = async function () {
+  modal.style.opacity = 0;
+  setTimeout(() => {
   modal.style.display = "none"
+  }, 500);
 }
 
 window.onclick = function (e) {
-  if (e.target === modal) modal.style.display = "none"
+  if (e.target === modal) {
+  modal.style.opacity = 0;
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, 600);
+}
 }
